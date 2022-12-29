f775534f1c668f2cbe3b40be686b44eca79a4dd1
mufeedppm <mufeedppm@gmail.com>
2022-12-30 00:49:46 +0530

    task9 js

--- GitStart/AssignmentTasks/main.js
+++ GitStart/AssignmentTasks/main.js
@@ -70,21 +70,3 @@ function filterItems(e){
     });
 }
 
-// function filterItems(e){
-//     // convert text to lowercase
-//     var text = e.target.value.toLowerCase();            var text=e.target.value.toLowerCase()
-//     // Get lis
-//     var items = itemList.getElementsByTagName('li');    var items=itemList.getElementsByTagName('li')
-
-//     // Convert to an array
-//     Array.from(items).forEach(function(item){           Array.from(items).forEach(function(item){
-//       var itemName = item.firstChild.textContent;           var itemName=item.firstChild.textContent;
-
-//       if(itemName.toLowerCase().indexOf(text) != -1){       if(itemName.toLowerCase().indexOf(text) != -1){
-//         item.style.display = 'block';                               item.style.display='block';}
-
-//       } else {                                              else{
-//         item.style.display = 'none';                        
-//       }
-//     });
-//   }
