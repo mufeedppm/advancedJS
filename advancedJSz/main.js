850e96a09c6445ddea76b7da8afd2ed4fa3fc373
mufeedppm <mufeedppm@gmail.com>
2023-01-17 11:28:51 +0530

    task12 js

--- advancedJSz/main.js
+++ advancedJSz/main.js
@@ -68,7 +68,7 @@ console.log(bound(1,1,1))
 
 
 //4
-let student={age:20}
+let student1={age:20}
 
 
 
@@ -111,3 +111,53 @@ let addTwo=add(2)
 
 
 addTwo(3);
+
+class student{
+
+ static i=0;
+
+ constructor(name,age,marks){
+
+  this.name=name;
+
+  this.age=age;
+
+  // this.phone=phone;
+
+  this.marks=marks;
+
+  student.i+=1;
+ }
+
+  eligibleForPlacement(minAge){
+      return (minMark) =>{
+          if(this.marks>=minMark && this.age>=minAge){
+            console.log(`${this.name} is  eligible for placement`)
+          }
+          else
+            console.log(`${this.name} is not eligible for placement`)
+    
+      
+     }
+  
+  
+}   
+  
+ 
+}
+let obj1=new student("akash",22,45)
+
+let obj2=new student("abhinav",20,54)
+
+let obj3=new student("aravind",24,45)
+
+let obj4=new student("amal",23,32)
+
+let obj5=new student("akshay",21,56)
+
+obj1.eligibleForPlacement(21)(45)
+obj2.eligibleForPlacement(21)(45)
+obj3.eligibleForPlacement(21)(45)
+obj4.eligibleForPlacement(21)(45)
+obj5.eligibleForPlacement(21)(45)
+  
