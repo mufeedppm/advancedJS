d0293a1a06e9971b8f6b9a0fc05430952c449375
mufeedppm <mufeedppm@gmail.com>
2023-01-27 20:08:37 +0530

    saving to crud instead of local storage

--- GitStart/BookingAppointment/app.js
+++ GitStart/BookingAppointment/app.js
@@ -8,6 +8,8 @@ var phoneInput=document.getElementById('phone')
 form.addEventListener('submit',addUser)
 // userList.addEventListener('click',removeUser)
 
+axios.get("https://crudcrud.com/api/eb39397dcb774e03980c8074be88b756/AppData")
+
 function addUser(e){
     e.preventDefault()
     var name=e.target.userName.value;
@@ -23,55 +25,70 @@ function addUser(e){
         alert("Please enter all fields")
     }
     else{
-        localStorage.setItem(obj.email, JSON.stringify(obj))
+        // localStorage.setItem(obj.email, JSON.stringify(obj))
+        axios.post("https://crudcrud.com/api/eb39397dcb774e03980c8074be88b756/AppData",obj)
+        .then((res)=>{
+            console.log(res.data)
+            
+        }).catch(err=>console.log(err))
+        
+    }
+    nameInput.value='';
+    emailInput.value='';
+    phoneInput.value=''
+}
+
+function ShowDetails(obj){
         var li=document.createElement('li')
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value},${phoneInput.value}`))
+        li.appendChild(document.createTextNode(`${obj.name} : ${obj.email},${obj.phone}`))
+        
         var delBtn=document.createElement('button')
 
-    delBtn.className='deleteBtn delete'
+        delBtn.className='deleteBtn delete'
 
-    delBtn.appendChild(document.createTextNode('X'))
+        delBtn.appendChild(document.createTextNode('X'))
 
-    li.appendChild(delBtn);
+        li.appendChild(delBtn);
 
-    delBtn.onclick = () =>{
-        if(confirm(`Press Ok to delete`)){
-        localStorage.removeItem(obj.email)
-        userList.removeChild(li)
-        }
-    }
-
-    var EditBtn=document.createElement('button')
-
-    EditBtn.className=' editBtn '
-
-    EditBtn.appendChild(document.createTextNode('Edit'))
-
-    li.appendChild(EditBtn);
-    
-    EditBtn.onclick = () =>{
-        if(confirm(`Press OK to confirm edit`)){
-            nameInput.value=obj.name;
-            emailInput.value=obj.email;
-            phoneInput.value=obj.phone;
+        delBtn.onclick = () =>{
+            if(confirm(`Press Ok to delete`)){
             localStorage.removeItem(obj.email)
             userList.removeChild(li)
+            }
         }
+        var EditBtn=document.createElement('button')
+
+        EditBtn.className=' editBtn '
+
+        EditBtn.appendChild(document.createTextNode('Edit'))
+
+        li.appendChild(EditBtn);
+        
+        EditBtn.onclick = () =>{
+            if(confirm(`Press OK to confirm edit`)){
+                nameInput.value=obj.name;
+                emailInput.value=obj.email;
+                phoneInput.value=obj.phone;
+                localStorage.removeItem(obj.email)
+                userList.removeChild(li)
+            }
+        }
+
+        userList.appendChild(li)
+    
     }
-
-
-    userList.appendChild(li)
+// ------------------------------------------------------------------------------------------------------------------
+    
+// -------------------------------------------------------------------------------------------------------------------
+   
 
     
     
     
         
 
-        nameInput.value='';
-        emailInput.value='';
-        phoneInput.value=''
-    }
-}
+  
+// --------------------------------------------------------------------------------------------------------
 function removeUser(e){
     if(e.target.classList.contains('delete')){
         if(confirm(`Confirm to delete`)){
@@ -90,26 +107,31 @@ function removeUser(e){
     }
 }
 
-function saveToLocal(e){
-    e.preventDefault();
-    var name=e.target.userName.value;
-    var email=e.target.email.value;
-    var phone=e.target.phone.value;
-    var obj={
-        name,
-        email,
-        phone}
+// function saveToCrud(e){
+//     e.preventDefault();
+//     var name=e.target.userName.value;
+//     var email=e.target.email.value;
+//     var phone=e.target.phone.value;
+//     var obj={
+//         name,
+//         email,
+//         phone}
     
-    if(nameInput.value=="" || emailInput.value==""|| phoneInput.value=="" ){
+//     if(nameInput.value=="" || emailInput.value==""|| phoneInput.value=="" ){
         
-    }
+//     }
     
-    else{   
+//     else{   
             
-            localStorage.setItem(obj.email, JSON.stringify(obj))
-            // var objDeserial=JSON.parse(localStorage.getItem(obj.email))
-            // console.log(objDeserial)
-        }
+//             axios.post("https://crudcrud.com/api/eb39397dcb774e03980c8074be88b756/AppData",obj)
+//             .then((res)=>{
+//                 console.log(res.data)
+                
+//             })
+//             // localStorage.setItem(obj.email, JSON.stringify(obj))
+//             // var objDeserial=JSON.parse(localStorage.getItem(obj.email))
+//             // console.log(objDeserial)
+//         }
     
-}
+// }
 
